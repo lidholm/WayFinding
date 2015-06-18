987f795e3133b36763a0a165443fd7149c958901
Andreas Lidholm <lidholm.andreas@gmail.com>
2015-06-18 09:07:58 +0200

    Set up to use Karma for unit testing and add a unit test for calculating distance between 2 points

--- wayFindingTest.js
+++ wayFindingTest.js
@@ -1,7 +1,7 @@
 
 
 describe('WayFindingCtrl', function() {
-  beforeEach(module('app'));
+  beforeEach(module('wayFindingApp'));
 
   var $controller;
 
@@ -18,7 +18,26 @@ describe('WayFindingCtrl', function() {
       var pointA = [1,0];
       var pointB = [4,0];
       var distance = $scope.distance(pointA, pointB);
-      expect(distance).toEqual(1);
+      expect(distance).toEqual(3);
     });
+    it('calculates the distance between two points on y axis', function() {
+        var $scope = {};
+        var controller = $controller('WayFindingCtrl', { $scope: $scope });
+        
+        var pointA = [0,3];
+        var pointB = [0,-3];
+        var distance = $scope.distance(pointA, pointB);
+        expect(distance).toEqual(6);
+      });
+
+    it('calculates the distance between two "random" points', function() {
+        var $scope = {};
+        var controller = $controller('WayFindingCtrl', { $scope: $scope });
+        
+        var pointA = [3,2];
+        var pointB = [9,7];
+        var distance = $scope.distance(pointA, pointB);
+        expect(distance).toEqual(7.810249675906654);
+      });
   });
 });
